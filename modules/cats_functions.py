8155d7f89ff1e62b69c4dd3d5db4e86cf46d0603
serii burduja <seriiburduja@gmail.com>
2024-12-22 18:44:21 +0200

    feat: catFunctions

---
+++ modules/cats_functions.py
@@ -0,0 +1,33 @@
+def catsFunctions():
+    def catCreate():
+        global power
+        power = 100
+        print("Cat created")
+
+    def catIsActive():
+        return power >= 0
+    
+    def catPlay():
+        global power
+        print(f'Поиграй со мной, {power}')
+        power -= int(input())
+
+    def catWantPlay():
+        global power
+        print(f' Поиграй со мной, {power}')
+
+    def catFeed():
+        global power
+        print(f' Покорми меня, {power}')
+        power += int(input())
+
+    def catSleep():
+        print('Я спать!')
+
+    catCreate()
+    while catIsActive():
+        catWantPlay()
+        catFeed()
+        catWantPlay()
+        catPlay()
+    catSleep()
