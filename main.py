8eb73973e965f0a639a9fe4458a6cba66b535661
serii burduja <seriiburduja@gmail.com>
2024-12-22 23:53:12 +0200

    feat: classes cats folder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from classes.BattleCat import BattleCat
-from classes.Cat import Cat
-from classes.Target import Target
+from classes.cats.BattleCat import BattleCat
+from classes.cats.Cat import Cat
+from classes.cats.Target import Target
 from modules.cats_functions import catsFunctions
 from modules.simple_cats import cat
 
