8c9f854bf1c48aaff30fc1219a7c437239bbd299
serii burduja <seriiburduja@gmail.com>
2024-12-23 00:10:48 +0200

    feat: employee class

---
+++ classes/employees/Employee.py
@@ -0,0 +1,6 @@
+class Employee:
+    def __init__(self, first_name: str, last_name: str, pay: int) -> None:
+        self._first_name = first_name
+        self._last_name = last_name
+        self._pay = pay
+        self._email = f"{first_name}.{last_name}@company.com"
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from classes.cats.BattleCat import BattleCat
-from classes.cats.Cat import Cat
-from classes.cats.Target import Target
-from modules.cats_functions import catsFunctions
-from modules.simple_cats import cat
+# from classes.cats.BattleCat import BattleCat
+# from classes.cats.Cat import Cat
+# from classes.cats.Target import Target
+# from modules.cats_functions import catsFunctions
+# from modules.simple_cats import cat
+#
+#
+# if __name__ == "__main__":
+#     # cat()
 
 
-if __name__ == "__main__":
-    # cat()
+from classes.employees.Employee import Employee
+
+employee_1 = Employee('serii', 'burduja', 30000)
+employee_2 = Employee('zezik', 'test', 30000)
