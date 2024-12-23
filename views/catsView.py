4e546897b57ac28906fcc2320d27be6ce394b9f3
serii burduja <seriiburduja@gmail.com>
2024-12-23 18:54:46 +0200

    feat: cats view

--- views/catsView.py
+++ views/catsView.py
@@ -6,4 +6,18 @@ from modules.simple_cats import cat
 
 
 def catsView():
-    cat()
+    # cat()
+    # catsFunctions()
+    # cat = Cat('Мурзик')
+    # while cat.is_active():
+    #     cat.feed()
+    #     cat.want_play()
+    #     cat.play()
+    # cat.sleep()
+    battle_cat = BattleCat('Барсик')
+    while battle_cat.is_active():
+        battle_cat.feed()
+        battle_cat.cannonFire()
+        battle_cat.want_play()
+        battle_cat.play()
+    battle_cat.sleep()
