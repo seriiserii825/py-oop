d339323271c927a9cfde078e2f608c29b33c36d1
serii burduja <seriiburduja@gmail.com>
2024-12-24 10:54:23 +0200

    feat: scissors finished

---
+++ modules/scissors_clear.py
@@ -0,0 +1,30 @@
+import random
+import sys
+from rich import print
+
+def scissorsClear():
+    playerchoice = input("Choose rock(1), paper(2), or scissors(3): ")
+    try:
+        playerchoice = int(playerchoice)
+    except ValueError:
+        sys.exit('Invalid type, please enter a number')
+
+    if playerchoice < 1 or playerchoice > 3:
+        sys.exit('Invalid choice')
+
+    computerchoice = random.randint(1, 3)
+    computerchoice = int(computerchoice)
+
+    print("Player choice: ", playerchoice)
+    print("Computer choice: ", computerchoice)
+
+    if playerchoice == computerchoice:
+        print("[blue]It's a tie!")
+    elif playerchoice == 1 and computerchoice == 3:
+        print("[green]Player wins!")
+    elif playerchoice == 2 and computerchoice == 1:
+        print("[green]Player wins!")
+    elif playerchoice == 3 and computerchoice == 2:
+        print("[green]Player wins!")
+    else:
+        print("[red]Computer wins!")
---
+++ modules/scissors_emojji.py
@@ -0,0 +1,35 @@
+from enum import Enum
+import random
+import sys
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+def scissorsEmojji():
+    playerchoice = input("🫠 Choose rock(1), paper(2), or scissors(3): ")
+    try:
+        playerchoice = int(playerchoice)
+    except ValueError:
+        sys.exit('👺 Invalid type, please enter a number')
+
+    if playerchoice < 1 or playerchoice > 3:
+        sys.exit('🧨 Invalid choice')
+
+    computerchoice = random.randint(1, 3)
+    computerchoice = int(computerchoice)
+
+    print("Player choice: ", RPS(playerchoice).name)
+    print("Computer choice: ", RPS(computerchoice).name)
+
+    if playerchoice == computerchoice:
+        print("🟰 It's a tie!")
+    elif playerchoice == 1 and computerchoice == 3:
+        print("🎉 Player wins!")
+    elif playerchoice == 2 and computerchoice == 1:
+        print("🎉 Player wins!")
+    elif playerchoice == 3 and computerchoice == 2:
+        print("🎉 Player wins!")
+    else:
+        print("👺 Computer wins!")
