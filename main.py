322fed02154d9d43279e12444d74e4ab4ba218bf
serii burduja <seriiburduja@gmail.com>
2025-06-14 00:07:37 +0300

    feat: small chages

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from modules.folium import my_folium
-
-
-my_folium()
+name = "Serii Burduja"
+print(name.rfind("urd"))
+# try:
+#     age = int(input("Enter your age: "))
+# except ValueError:
+#     print("Invalid input. Please enter a number.")
+#     age = None
+#
+# print(f"age: {age}")
---
+++ views/guess_rectange_point.py
@@ -0,0 +1,5 @@
+from classes.guess_rectange_point.Point import Point
+
+
+def guess_rectange_point():
+    point = Point(10, 20)
