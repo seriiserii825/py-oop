dbd2502a6e1c7e614bc4935e08511b653b9a7c83
serii burduja <seriiburduja@gmail.com>
2025-06-16 10:21:11 +0300

    feat: input validator

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
+from InputValidator import InputValidator
+
+
 my_object = {}
 
+
 def set_key() -> str:
-    """Set a key in the my_object dictionary."""
     while True:
-        key = input("Enter a key: ")
-        if not key:
-            print("Key cannot be empty. Please try again.")
-            continue
-        if key in my_object:
-            print(f"Key '{key}' already exists. Please enter a different key.")
-            continue
-        my_object[key] = None
-        return key
+        key = InputValidator.get_string("Enter a key: ", allow_empty=False)
+        if key not in my_object:
+            return key
+        print(f"Key '{key}' already exists. Please enter a different key.")
+
 
 def set_value(key: str) -> None:
-    """Set a value for the given key in the my_object dictionary."""
-    while True:
-        value = input(f"Enter a value for '{key}': ")
-        if not value:
-            print("Value cannot be empty. Please try again.")
-            continue
-        my_object[key] = value
-        return
+    value = InputValidator.get_string(
+        f"Enter a value for '{key}': ", allow_empty=False)
+    my_object[key] = value
+
 
 for _ in range(3):
     key = set_key()
