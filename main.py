bc5e9513991d409b1641e666e48c4e2d338d0784
serii burduja <seriiburduja@gmail.com>
2025-06-19 17:34:06 +0300

    feat: set

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
-from InputValidator import InputValidator
-
-
-my_object = {}
-
-
-def set_key() -> str:
-    while True:
-        key = InputValidator.get_string("Enter a key: ", allow_empty=False)
-        if key not in my_object:
-            return key
-        print(f"Key '{key}' already exists. Please enter a different key.")
-
-
-def set_value(key: str) -> None:
-    value = InputValidator.get_string(
-        f"Enter a value for '{key}': ", allow_empty=False)
-    my_object[key] = value
-
-
-for _ in range(3):
-    key = set_key()
-    set_value(key)
-
-print(f'my_object: {my_object}')
+fruits = ["apple", "banana", "cherry"]
+print(f"fruits: {fruits}")
+first_letter = [fruit[0] for fruit in fruits]
+first_letter_unique = set(first_letter)
+print(f"first_letter_unique: {first_letter_unique}")
